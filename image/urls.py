f8f1bcc12e971ad1c566092f86e918ace11fdd92
Yuvanth <sadamyuvanth@gmail.com>
2022-05-26 11:46:57 +0530

    changed in url

--- image/urls.py
+++ image/urls.py
@@ -4,10 +4,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from upload.views import proListView
-from upload import views
+# from upload import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('profile', proListView.as_view(), name='pro-list'),
-    path('', views.index,name = 'index'),
+    # path('', views.index,name = 'index'),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
