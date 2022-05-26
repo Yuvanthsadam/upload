633de8a96c55e7eb90d506b4724ed28cade066c0
Yuvanth <sadamyuvanth@gmail.com>
2022-05-26 12:44:25 +0530

    changed in urls

--- upload/urls.py
+++ upload/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 from upload.views import proListView, proDetailView
 
 path('profile', proListView.as_view(), name='pro-list'),
-path('list/<int:pk>', proDetailView.as_view(), name='emp-detail'),
+path('profile/<int:pk>', proDetailView.as_view(), name='pro-detail'),
