3fa6e3b34bb19e2e3921681870fe09b89607979f
Yuvanth <sadamyuvanth@gmail.com>
2022-05-26 12:50:56 +0530

    changed in urls

--- image/urls.py
+++ image/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url,include
+# from django.conf.urls import url,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from upload.views import proListView, proDetailView
+# from upload import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^',include('upload.urls'))
+    path('profile', proListView.as_view(), name='pro-list'),
+    path('profile/<int:pk>', proDetailView.as_view(), name='pro-detail'),
+    # path('', views.index,name = 'index'),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- upload/urls.py
+++ upload/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path
-from upload.views import proListView, proDetailView
+# from django.urls import path
+# from upload.views import proListView, proDetailView
 
-path('profile', proListView.as_view(), name='pro-list'),
-path('profile/<int:pk>', proDetailView.as_view(), name='pro-detail'),
+# path('profile', proListView.as_view(), name='pro-list'),
+# path('profile/<int:pk>', proDetailView.as_view(), name='pro-detail'),
